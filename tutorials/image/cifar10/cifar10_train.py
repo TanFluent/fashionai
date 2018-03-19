f9ba2d16fe50ec9e0b4a00b1319be9d49186c334
tanfulun <swat_tfl@sina.com>
2018-03-19 15:18:22 +0800

    my branch first commit on 9505

--- tutorials/image/cifar10/cifar10_train.py
+++ tutorials/image/cifar10/cifar10_train.py
@@ -45,10 +45,10 @@ import cifar10
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
+tf.app.flags.DEFINE_string('train_dir', './cifar_data',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 1000000,
+tf.app.flags.DEFINE_integer('max_steps', 10000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
--- tutorials/image/imagenet/classify_image.py
+++ tutorials/image/imagenet/classify_image.py
@@ -204,7 +204,7 @@ if __name__ == '__main__':
   parser.add_argument(
       '--model_dir',
       type=str,
-      default='/tmp/imagenet',
+      default='./material',
       help="""\
       Path to classify_image_graph_def.pb,
       imagenet_synset_to_human_label_map.txt, and
